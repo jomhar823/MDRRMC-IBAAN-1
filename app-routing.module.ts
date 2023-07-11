c3f35a481404e0513504f334ec34124fbd48a2d6
ForlajeMiles <137494451+ForlajeMiles@users.noreply.github.com>
2023-07-11 17:12:10 +0800

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'',
+    loadChildren:() => import('./home/home.module').then(m=> m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'announcement',
+    loadChildren: () => import('./page/announcement/announcement.module').then( m => m.AnnouncementPageModule)
+  },
+  {
+    path: 'reports',
+    loadChildren: () => import('./page/reports/reports.module').then( m => m.ReportsPageModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
+  }
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
